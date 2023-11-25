fb992ebf6efe5c9f564c2c772ac73efe622690b4
Jonathan McClen <jonathangmcclen@gmail.com>
2023-11-25 11:25:29 +0000

    Added testing functionality and Adam  to LinrModel

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,55 +1,69 @@
 import numpy as np
 
 class LinearModel:
-    
+
+    def test(self,featureMatrix,outcomeVector):
+
+        predictions = self.predict(featureMatrix)
+        absoluteError = np.absolute(outcomeVector-predictions)
+        self.absoluteError = absoluteError/np.size(outcomeVector)
+        return f'Model testing complete current absolute error is {self.absoluteError}.'
+
     def report_model_status(self):
         return f'{self.convergency_status}\nModel contains {self.number_of_features} features.\nCurrent weights for model are {self.weights}.'
     
-    def fit(self, featureMatrix, outputVector):
-        
-        #Set default learning rate
-        learningRate = 0.1
+    def fit(self, featureMatrix, outputVector, learningRate, accuracyGoal, maxIterations):
         
         #Check that number of features attribute is not yet created
         if hasattr(self, 'number_of_features'):
-            Exception('Fit method not available for pre-trained models')
+            raise Exception('Fit method not available for pre-trained models')
         
         #Get number of features from width of featureMatrix
         self.number_of_features = np.shape(featureMatrix)[1]
         
         #Create vector of all weights
-        self.weights = np.matrix(np.linspace(0.1,0.1,self.number_of_features))
+        self.weights = np.matrix(np.random.rand(self.number_of_features))
         
-        total_of_all_avg_gradients = 0
+        #Adam implementation
+        m = np.zeros_like(self.weights)
+        v = np.zeros_like(self.weights)
+        beta1 = 0.9  # Exponential decay rates for moment estimates
+        beta2 = 0.999
+        epsilon = 1e-8  # Small constant to prevent division by zero
+
         iterations = 1
         
         while True:
             
             #Calculate partial derivatives
-            dLdw = -2 * (outputVector - featureMatrix*self.weights.T).T * featureMatrix
+            try:
+                dLdw = -2 * (outputVector - featureMatrix*self.weights.T).T * featureMatrix
+            except:
+                return f'Error in gradient calculation, model did not converge on iteration {iterations}.'
             
-            #Adjust learning rate if diverging
+            #Calculate new average gradient
             current_avg_gradient = np.mean(np.absolute(dLdw))
-            if current_avg_gradient > total_of_all_avg_gradients/iterations:
-                learningRate /= 2
-            
-            #Update all gradients sum placeholder
-            total_of_all_avg_gradients += current_avg_gradient
             
             #Test stop conditions
-            if iterations > 10000:
+            if iterations > maxIterations:
                 self.convergency_status = f'Non converging after {iterations} iterations.'
                 return 'Model did not converge'
             
-            if current_avg_gradient < 0.001:
+            if current_avg_gradient < accuracyGoal:
                 self.convergency_status = f'Converged after {iterations} iterations.'
                 return self.convergency_status
             
-            #Update weights using the partial derivatives
-            self.weights -= dLdw * learningRate
+            # Adam update rules
+            m = beta1 * m + (1 - beta1) * dLdw
+            v = beta2 * v + (1 - beta2) * (dLdw ** 2)
+            m_hat = m / (1 - beta1 ** iterations)
+            v_hat = v / (1 - beta2 ** iterations)
+
+            #Update weights
+            self.weights -= learningRate * m_hat / (np.sqrt(v_hat) + epsilon)
             
             iterations += 1
             
     #Make prediction using the model can also take matrix for multiple predictions
-    def predict(self, inputVector):
-        return inputVector * self.weights.T
+    def predict(self, featureMatrix):
+        return featureMatrix * self.weights.T
